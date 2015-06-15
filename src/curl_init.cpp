1b5d7d6e53c0e5a32795847ad6f5aa6b1843f5b5
Frederik Rodrigue <fredrod@users.noreply.github.com>
2015-06-15 13:04:02 -0400

    Removed debug logs.

--- src/curl_init.cpp
+++ src/curl_init.cpp
@@ -33,7 +33,6 @@ void curl_init::init(const long flag) {
 // Implementation of curl_init default Constructor
 curl_init::curl_init() {
   flag = CURL_GLOBAL_ALL;
-  cout << "curl_global_init" << endl;
   const CURLcode code = curl_global_init(flag);
   if (code != CURLE_OK) {
     throw curl_easy_exception(code, __FUNCTION__);
@@ -43,7 +42,6 @@ curl_init::curl_init() {
 // Implementation of curl_init Constructor
 curl_init::curl_init(const long flag) {
   this->flag = flag;
-  cout << "curl_global_init" << endl;
   const CURLcode code = curl_global_init(flag);
   if (code != CURLE_OK) {
     throw curl_easy_exception(code, __FUNCTION__);
@@ -52,16 +50,13 @@ curl_init::curl_init(const long flag) {
 
 // Implementation of curl_init Destructor
 curl_init::~curl_init() {
-  cout << "curl_global_cleanup" << endl;
   curl_global_cleanup();
 }
 
 void curl_init::reset() {
-  cout << "curl_global_cleanup" << endl;
   curl_global_cleanup();
 
   flag = CURL_GLOBAL_ALL;
-  cout << "curl_global_init" << endl;
   const CURLcode code = curl_global_init(flag);
   if (code != CURLE_OK) {
     throw curl_easy_exception(code, __FUNCTION__);
@@ -69,11 +64,9 @@ void curl_init::reset() {
 }
 
 void curl_init::reset(const long flag) {
-  cout << "curl_global_cleanup" << endl;
   curl_global_cleanup();
 
   this->flag = flag;
-  cout << "curl_global_init" << endl;
   const CURLcode code = curl_global_init(flag);
   if (code != CURLE_OK) {
     throw curl_easy_exception(code, __FUNCTION__);
